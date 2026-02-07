a7922b4d7bee9cf4e89b5735e4625dfd956fadfb
Dayananda-Sharma <dayanandsharma.x-workz@outlook.com>
2026-02-07 18:02:41 +0530

    project set

---
+++ rolex/src/main/java/com/xworkz/rolex/external/WatchRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.rolex.external;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class WatchRunner {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("rolex");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+    }
+}
